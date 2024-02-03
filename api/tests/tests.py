9ae44f79553123ae648c1b9964945a9f8267e3ba
guimarczewski <guimarczewski@gmail.com>
2024-02-03 17:25:32 +0000

    create new tests in api routes

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -13,3 +13,21 @@ def test_ola_mundo():
 def test_listar_produtos_status_code():
     response = client.get("/produtos")
     assert response.status_code == 200
+
+def test_create_product():
+    product_data = {"id": 1, "titulo": "Cadeira Gamer", "descricao": "Cadeira confortável para fazer live", "preco": 1200.00}
+    response = client.post("/produtos", json=product_data)
+    assert response.status_code == 200
+
+def test_read_product_by_id():
+    response = client.get("/produtos/1")
+    assert response.status_code == 200
+
+def test_update_product():
+    product_data = {"id": 1, "titulo": "Cadeira Gamer", "descricao": "Cadeira confortável para fazer live", "preco": 1500.00}
+    response = client.put("/products/1", json=product_data)
+    assert response.status_code == 200
+
+def test_delete_product():
+    response = client.delete("/products/1")
+    assert response.status_code == 200
